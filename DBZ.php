d4e15aa309ddbb95fc922cd552d78f5606f12f84
unknown <y.dahmouh@ecole-ipssi.net>
2023-10-26 11:37:46 +0200

    creation de class

--- DBZ.php
+++ DBZ.php
@@ -6,8 +6,14 @@
       
     }
     
-    
-    echo "bonjour"
+    public function __construct($nom, $level, $pv) {
+        $this->nom = $nom;
+        $this->niveau_de_puissance = $niveau_de_puissance;
+        $this->points_de_vie = $points_de_vie;
+    }
+    class hero extends personnage {
+
+    }
     
 
 ?>
