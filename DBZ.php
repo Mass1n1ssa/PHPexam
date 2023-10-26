6c7e91a427a716edf1e7e7b501d7fd4faa0ece72
Massinissa AMANZOUGAGHENE <m.amanzougaghene@ecole-ipssi.net>
2023-10-26 11:57:58 +0200

    MAJ

--- DBZ.php
+++ DBZ.php
@@ -1,4 +1,22 @@
 <?php 
-    echo "despee";
+    Class personnage{
+        protected $nom;
+        protected $puissance;
+        protected $vie;
+
+        public function __construct($nom,$puissance,$vie){
+            $this->nom = $nom;
+            $this->puissance = $puissance;
+            $this->vie = $vie;
+        }
+    }
+
+    Class heros extends personnage{
+        public function __construct($nom,$puissance,$vie){
+            parent::__construct($nom,$puissance,$vie);
+        }
+    }
+
+    
 
 ?>
