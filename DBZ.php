f2edcdca030104d2e19f5c1e00d148d297c4e998
unknown <y.dahmouh@ecole-ipssi.net>
2023-10-26 12:38:51 +0200

    Maj

--- DBZ.php
+++ DBZ.php
@@ -3,43 +3,14 @@
         public $name;
         public $powerLevel;
         public $hp;
-<<<<<<< HEAD
     }
+
     
     public function __construct($name, $powerLevel, $hp) {
         $this->name = $name;
         $this->powerLevel = $powerLevel;
         $this->hp = $hp;
     }
-
-=======
-
-        public function __construct($name, $powerLevel, $hp) {
-                $this->name = $name;
-                $this->powerLevel = $powerLevel;
-                $this->hp = $hp;
-
-        }
-    }
-    
-    
-
-    class Hero extends Character {
-        public function __construct($name, $powerLevel, $hp) {
-            parent::__construct($name, $powerLevel, $hp);
-        }
-
-        public function attack($target) {
-            $target->hp -= $this->powerLevel;
-            
-            if ($target->hp <= 0) {
-                echo $target->name . " est mort !";
-            } else {
-                echo $target->name . " a survécu !";
-            }
-        }
-    }
->>>>>>> edc31b4738ec1738017211e879ded9ffe7f5278d
 
     class Heros extends Character {
 
@@ -57,5 +28,15 @@ class Evil extends Character {
         $this->special = $special;
     }
 }
+ public function attack($target) {
+        $target->hp -= $this->powerLevel;
+        if ($target->hp <= 0) {
+            echo $target->name . " est mort !";
+        } else {
+            echo $target->name . " a survécu !";
+        }
+    }
+$goku = New user ("goku",100,1000)
+$vegeta = New user ("goku"100,900)
 
 ?>
