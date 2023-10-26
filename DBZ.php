dafe4cfa0d213385b84d0afd0856bb1ea7a0f728
Mass1n1ssa <m.amanzougaghene@ecole-ipssi.net>
2023-10-26 14:27:34 +0200

    MAJ

--- DBZ.php
+++ DBZ.php
@@ -1,41 +1,72 @@
-<?php 
-    class Character 
+<?php
+class Character
+{
+    protected string $name;
+    protected int $ki;
+    protected int $hp;
+    protected int $damage;
+
+    public function __construct($name, $ki, $hp, $damage)
     {
-        protected string $name;
-        protected int $powerLevel;
-        protected int $hp;
-        protected int $damage;
-
-        public function __construct($name, $powerLevel, $hp, $damage)
-        {
-            $this->name = $name;
-            $this->powerLevel = $powerLevel;
-            $this->hp = $hp;
-            $this->damage = $damage;
-        }
+        $this->name = $name;
+        $this->ki = $ki;
+        $this->hp = $hp;
+        $this->damage = $damage;
     }
+
+    public function attack($target)
+    {
+        $target->hp -= $this->damage;
+        $this->ki += 10;
+        echo $this->name . " attaque " . $target->name . " et lui inflige " . $this->damage . " points de dégats ! \n" . $this->name . " a maintenant " . $this->ki . " points de puissance ! \n";
+        echo $target->name . " a maintenant " . $target->hp . " points de vie ! \n";
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getKi()
+    {
+        return $this->ki;
+    }
+
+    public function getHp()
+    {
+        return $this->hp;
+    }
+}
+
+class Hero extends Character
+{
+    public function __construct($name, $ki, $hp, $damage)
+    {
+        parent::__construct($name, $ki, $hp, $damage);
+    }
+}
+
+class Evil extends Character
+{
     
-    class Hero extends Character {
-        public function __construct($name, $powerLevel, $hp) {
-            parent::__construct($name, $powerLevel, $hp);
-        }
 
-        public function attack($target) {
-            $target->hp -= $this->damage;
-            $this->powerLevel += 10;
-            echo $this->name . " attaque " . $target->name . " et lui inflige " . $this->$damage . " points de dégats ! <br>" . $this->name . " a maintenant " . $this->powerLevel . " points de puissance ! <br>" ;
-        }
+    public function __construct($name, $ki, $hp, $damage)
+    {
+        parent::__construct($name, $ki, $hp, $damage);
+        
     }
+}
 
-    class Evil extends Character {
-    
-        public function __construct($name, $powerlevel, $hp, $special) {
-            parent::__construct($name, $powerlevel, $hp);
-            $this->special = $special;
-        }
-    }
+$goku = new Hero("Goku", 9000, 100, 10);
+$vegeta = new Evil("Vegeta", 8000, 150, 15);
 
-    $goku = new Hero("Goku", 9000, 100, 10);
-    $vegeta = new Hero("Vegeta", 8000, 150, 15);
+// Test
+echo "Avant l'attaque : \n";
+echo $goku->getName() . " a " . $goku->getKi() . " points de puissance et " . $goku->getHp() . " points de vie.\n";
+echo $vegeta->getName() . " a " . $vegeta->getKi() . " points de puissance et " . $vegeta->getHp() . " points de vie.\n";
 
-?>
+echo "\nAprès l'attaque :\n";
+$goku->attack($vegeta);
+
+echo "\nAprès l'attaque de Vegeta :\n";
+$vegeta->attack($goku);
