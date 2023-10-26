7db048570a97b0945d3e7e455627827bc5c2b5dc
unknown <y.dahmouh@ecole-ipssi.net>
2023-10-26 12:24:40 +0200

    maj

--- DBZ.php
+++ DBZ.php
@@ -3,15 +3,6 @@
         public $name;
         public $powerLevel;
         public $hp;
-
-        public function attack($target) {
-            $target->hp -= $this->powerLevel;
-            if ($target->hp <= 0) {
-                echo $target->name . " est mort !";
-            } else {
-                echo $target->name . " a survécu !";
-            }
-        }
     }
     
     public function __construct($name, $powerLevel, $hp) {
@@ -20,21 +11,22 @@
         $this->hp = $hp;
     }
 
-    class Hero extends Character {
-        public function __construct($name, $powerLevel, $hp) {
-            parent::__construct($name, $powerLevel, $hp);
-        }
-    }
 
+    class Heros extends Character {
+
+    public function __construct($name, $powerlevel, $, $special) {
+        parent::__construct($name, $powerlevel, $hp);
+        $this->special = $special;
+    }
    
+}
 
-
-    class Evil extends Character {
-    
-        public function __construct($name, $powerlevel, $hp, $special) {
-            parent::__construct($name, $powerlevel, $hp);
-            $this->special = $special;
-        }
+class Evil extends Character {
+  
+    public function __construct($name, $powerlevel, $hp, $special) {
+        parent::__construct($name, $powerlevel, $hp);
+        $this->special = $special;
     }
+}
 
 ?>
